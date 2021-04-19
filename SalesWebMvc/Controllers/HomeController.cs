052b93f635a5b1d5b5ce7d263bcbf072ca9e926a
Carlosmvsep3@hotmail.ccom <Carlosmvsep3@hotmail.com>
2021-04-19 14:42:36 -0300

    First controller and razos pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App From c#";
+            ViewData["Aluno"] = "Carlos Henrique Lima Cicoti do curso C# do Nelio Alves";
+
 
             return View();
         }
