db07f5f21af0003f4c158aa142d69ba9900479c8
Carlosmvsep3@hotmail.ccom <Carlosmvsep3@hotmail.com>
2021-04-22 10:31:37 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 
 namespace SalesWebMvc
@@ -42,7 +43,7 @@ namespace SalesWebMvc
                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                      builder.MigrationsAssembly("SalesWebMvc")));
             services.AddScoped<SeedingService>();
-
+            services.AddScoped<SellerService>();
 
         }
 
